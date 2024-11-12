9e6b317b4c4ad0861d86e78c565dd900df8f83db
Edwin Oswaldo Torres Rincón <edwin.torres.r2000@gmail.com>
2024-11-11 22:33:47 -0500

    Se actualiza el archivo boa_constrictor.py para probar los cambios en el git

--- boa_constrictor.py
+++ boa_constrictor.py
@@ -10,7 +10,7 @@ class Boa_Constrictor(Animal_Exotico):
         return "¡Tsss!"
 
     def agregar_raton(self):
-        if self.ratones_comidos >= 10:
+        if self.ratones_comidos >= 20:
             raise ValueError("Demasiados Ratones!")
         self.ratones_comidos += 1
 
